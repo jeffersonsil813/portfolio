996cb2520d0d33e7631d1d8b42ded3cb08e6b5c6
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-03-22 10:23:43 -0300

    Add variant and default text color on text component

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -1,12 +1,13 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import React, { type JSX } from "react";
 
-export const textVariants = cva("font-sans", {
+export const textVariants = cva("font-sans text-indigo-900", {
   variants: {
     variant: {
       "body-sm-bold": "text-sm leading-5 font-semibold",
       "body-md": "text-base leading-6 font-normal",
       "body-md-bold": "text-base leading-6 font-semibold",
+      "body-lg-bold": "text-lg leading-6 font-semibold",
       "body-xl": "text-9xl leading-10",
     },
   },
