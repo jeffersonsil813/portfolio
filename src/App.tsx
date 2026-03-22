13a4f86c2a1a77905568c48a6728927142712dac
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-03-22 09:10:10 -0300

    Create the hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import Hero from "./components/hero";
 import Navbar from "./components/navbar";
 import "./i18n/i18n";
 
 const App = () => {
-
-  return <Navbar />;
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
 };
 
 export default App;
---
+++ src/components/hero/index.tsx
@@ -0,0 +1,5 @@
+const Hero = () => {
+  return <div className="min-h-screen bg-linear-to-br from-zinc-200 via-purple-50 to-zinc-50">oi</div>;
+};
+
+export default Hero;
