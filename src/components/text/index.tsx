e6eedf18dffa3750a4d02f557bf93a2aa4b1cfa4
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-03-22 08:19:54 -0300

    Remove text-white

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -1,7 +1,7 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import React, { type JSX } from "react";
 
-export const textVariants = cva("font-sans text-white", {
+export const textVariants = cva("font-sans", {
   variants: {
     variant: {
       "body-sm-bold": "text-sm leading-5 font-semibold",
