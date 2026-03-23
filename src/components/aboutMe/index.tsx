4988750063bec033658688235d2c74f53ee8ef9d
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-03-23 20:19:10 -0300

    Add AboutMe component with image and translations

--- src/components/aboutMe/index.tsx
+++ src/components/aboutMe/index.tsx
@@ -1,5 +1,29 @@
+import MePhoto from "../../assets/me.png";
+import { useTypedTranslation } from "../../hooks/useTypedTranslation";
+import Text from "../text";
+
 const AboutMe = () => {
-  return <section id="me" className="min-h-screen bg-linear-to-br from-zinc-200 via-purple-50 to-zinc-50"></section>;
+  const { t } = useTypedTranslation();
+
+  return (
+    <section
+      id="me"
+      className="relative min-h-screen bg-linear-to-br from-zinc-200 via-purple-50 to-zinc-50 pt-32 px-8 lg:px-16 flex flex-col lg:flex-row items-center justify-center gap-5 overflow-hidden"
+    >
+      <div className="z-10 flex flex-1 flex-col gap-5 min-w-0 text-center lg:text-start">
+        <Text variant="body-7xl">{t("aboutMe.title")}</Text>
+        <Text variant="body-xl">{t("aboutMe.subtitle")}</Text>
+      </div>
+
+      <div className="flex-1 w-full flex justify-center lg:justify-end items-end self-end">
+        <img
+          src={MePhoto}
+          alt="Jeff"
+          className="w-auto h-auto max-h-[50vh] lg:max-h-[80vh] object-contain object-bottom mix-blend-multiply"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default AboutMe;
